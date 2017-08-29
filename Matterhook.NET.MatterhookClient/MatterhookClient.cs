fd44e41b076c06eb91f2d1f1b9ed2d24495f4cb0
Adam Warner <adamw@rner.email>
2017-08-29 20:17:53 +0100

    remove unused dependencies

--- Matterhook.NET.MatterhookClient/MatterhookClient.cs
+++ Matterhook.NET.MatterhookClient/MatterhookClient.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
